4996a8452685867f0edee02c4c08b9d4efd3ec37
Maciej Stramski <mstramski@gmail.com>
2017-07-02 14:50:51 +0200

    Change conveninece method HEX_TO_UINT16 to actually create a 16 bit value out of arguments bytes.
    Changed Command signature to take always four bytes of data as value
    
    formatting e.g. ""R%dZ%04X\n"

--- Arduino/chorus-rf-laptimer/chorus-rf-laptimer.ino
+++ Arduino/chorus-rf-laptimer/chorus-rf-laptimer.ino
@@ -533,7 +533,7 @@ void handleSerialControlInput(uint8_t *controlData, uint8_t length) {
                 isConfigured = 1;
                 break;
             case CONTROL_SET_THRESHOLD_VALUE:
-                rssiThreshold = HEX_TO_UINT16(controlData[1], controlData[2], controlData[3]);
+                rssiThreshold = HEX_TO_UINT16(controlData[1], controlData[2], controlData[3], controlData[4]);
                 isConfigured = 1;
                 addToSendQueue(SEND_THRESHOLD);
                 break;
--- Arduino/chorus-rf-laptimer/sendSerialHex.h
+++ Arduino/chorus-rf-laptimer/sendSerialHex.h
@@ -35,8 +35,8 @@ uint8_t HEX_TO_BYTE (uint8_t hi, uint8_t lo) {
     return TO_BYTE(hi)*16+TO_BYTE(lo);
 }
 
-int16_t HEX_TO_UINT16 (uint8_t v3, uint8_t v2, uint8_t v1) {
-    return TO_BYTE(v3)*256+TO_BYTE(v2)*16+TO_BYTE(v1);
+int16_t HEX_TO_UINT16 (uint8_t v4, uint8_t v3, uint8_t v2, uint8_t v1) {
+    return TO_BYTE(v4)*4096+TO_BYTE(v3)*256+TO_BYTE(v2)*16+TO_BYTE(v1);
 }
 
 int32_t HEX_TO_SIGNED_LONG (uint8_t * buf) {
