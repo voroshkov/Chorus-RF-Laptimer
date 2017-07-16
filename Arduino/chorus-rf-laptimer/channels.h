6e93f4fda7266c738d89d76a6127a47b77133960
anunique <anunique@gmail.com>
2017-07-16 10:59:44 +0200

    set rssithreshold, set frequency
    first commit was a fail so heres another try:
    
    you cant set rssithreshold now
    you can set any frequency now

--- Arduino/chorus-rf-laptimer/channels.h
+++ Arduino/chorus-rf-laptimer/channels.h
@@ -27,6 +27,8 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
 
+#define MAX_BAND 7
+
 // Channels to send to the SPI registers
 const uint16_t channelTable[] PROGMEM = {
     // Channel 1 - 8
@@ -35,17 +37,21 @@ const uint16_t channelTable[] PROGMEM = {
     0x2903, 0x290C, 0x2916, 0x291F, 0x2989, 0x2992, 0x299C, 0x2A05, // Band B
     0x2895, 0x288B, 0x2881, 0x2817, 0x2A0F, 0x2A19, 0x2A83, 0x2A8D, // Band E
     0x2906, 0x2910, 0x291A, 0x2984, 0x298E, 0x2998, 0x2A02, 0x2A0C, // Band F / Airwave
-    0x2609, 0x261C, 0x268E, 0x2701, 0x2713, 0x2786, 0x2798, 0x280B // Band D / 5.3
+    0x2609, 0x261C, 0x268E, 0x2701, 0x2713, 0x2786, 0x2798, 0x280B, // Band D / 5.3
+    0x248f, 0x2499, 0x2503, 0x250d, 0x2909, 0x2913, 0x291d, 0x2987, //connex
+    0x2991, 0x299b, 0x299b, 0x299b, 0x299b, 0x299b, 0x299b, 0x299b  //even more connex, last 6 unused!!!    
 };
 
 // Channels' MHz Values. Just for reference. Not used in code.
 //
-// const uint16_t channelFreqTable[] PROGMEM = {
+ const uint16_t channelFreqTable[] PROGMEM = {
 //     // Channel 1 - 8
-//     5658, 5695, 5732, 5769, 5806, 5843, 5880, 5917, // Raceband
-//     5865, 5845, 5825, 5805, 5785, 5765, 5745, 5725, // Band A
-//     5733, 5752, 5771, 5790, 5809, 5828, 5847, 5866, // Band B
-//     5705, 5685, 5665, 5645, 5885, 5905, 5925, 5945, // Band E
-//     5740, 5760, 5780, 5800, 5820, 5840, 5860, 5880, // Band F / Airwave
-//     5362, 5399, 5436, 5473, 5510, 5547, 5584, 5621 // Band D / 5.3
-// };
+     5658, 5695, 5732, 5769, 5806, 5843, 5880, 5917, // Raceband
+     5865, 5845, 5825, 5805, 5785, 5765, 5745, 5725, // Band A
+     5733, 5752, 5771, 5790, 5809, 5828, 5847, 5866, // Band B
+     5705, 5685, 5665, 5645, 5885, 5905, 5925, 5945, // Band E
+     5740, 5760, 5780, 5800, 5820, 5840, 5860, 5880, // Band F / Airwave
+     5362, 5399, 5436, 5473, 5510, 5547, 5584, 5621, // Band D / 5.3
+     5180, 5200, 5220, 5240, 5745, 5765, 5785, 5805, // connex
+     5825, 5845, 5845, 5845, 5845, 5845, 5845, 5845  // even more connex, last 6 unused!!!
+ };
