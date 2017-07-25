a5aa715f19d9eb5564440e7e37d7380aece203b8
anunique <anunique@gmail.com>
2017-07-25 20:25:55 +0200

    baud detection using input pins
    baud detection using input pins

--- Arduino/chorus-rf-laptimer/chorus-rf-laptimer.ino
+++ Arduino/chorus-rf-laptimer/chorus-rf-laptimer.ino
@@ -52,8 +52,6 @@ uint8_t MODULE_ID_HEX = '0';
 
 #include <avr/pgmspace.h>
 
-#define BAUDRATE 115200
-
 const uint16_t musicNotes[] PROGMEM = { 523, 587, 659, 698, 784, 880, 988, 1046 };
 
 // number of analog rssi reads to average for the current check.
@@ -216,6 +214,9 @@ uint8_t proxyBufDataSize = 0;
 
 // ----------------------------------------------------------------------------
 void setup() {
+    // initialize digital pin 2 as an input.
+    pinMode(baudpin, INPUT);
+    
     // initialize digital pin 13 LED as an output.
     pinMode(led, OUTPUT);
     digitalHigh(led);
@@ -233,7 +234,15 @@ void setup() {
     // set the channel as soon as we can
     // faster boot up times :)
     setChannelModule(channelIndex, bandIndex, 0);
-    Serial.begin(BAUDRATE);
+    
+    if (digitalRead(baudpin) == HIGH)
+    {
+            Serial.begin(9600);
+    }
+    else
+    {
+      Serial.begin(115200);    
+    }
 
     initFastADC();
 
